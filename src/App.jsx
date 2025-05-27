0407d6f94d429cc03eb42cf82fa97691456af817
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2025-05-27 22:33:05 +0800

    second commit, adding some visual styling on the result and header area

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import React, { useState } from 'react';
-import './App.css';
-import Spotify from './Spotify'; // adjust the path if needed
+import React, { useState } from "react";
+import "./App.css";
+import Spotify from "./Spotify"; // adjust the path if needed
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
 
   const handleSearch = async () => {
     const tracks = await Spotify.search(searchTerm);
     setResults(tracks);
+    console.log(results);
   };
 
   return (
     <div>
-      <h1>Spotify Search Test</h1>
+      <h1 className="app-title">
+        Ja<span className="trademark">mmm</span>ing
+      </h1>
+      <br />
       <input
         type="text"
-        placeholder="Search songs"
+        placeholder="Search artist or track"
         value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
       />
+
       <button onClick={handleSearch}>Search</button>
-      <ul>
-        {results.map(track => (
-          <li key={track.id}>
-            {track.name} by {track.artist}
+      <br />
+      <br />
+      <ul className="results-list">
+        <h2>Results</h2>
+        {results.map((track) => (
+          <li key={track.id} className="track-item">
+            <div>
+              {track.name} by {track.artist}
+            </div>
+            <button>+</button>
           </li>
         ))}
       </ul>
