db830fc2b5f71324fb9ee987911a44c18ef78e24
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2025-05-28 22:47:41 +0800

    fourth commit, adding add and remove functionality, some layout align

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ import Spotify from "./Spotify"; // adjust the path if needed
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
-   console.log(results);
+  const [myPlaylist, setMyPlaylist] = useState([]);
+
   const handleSearch = async () => {
     const tracks = await Spotify.search(searchTerm);
     setResults(tracks);
-   
+  };
+
+  const addItem = (track) => {
+    const newTrack = results.filter((result) => result.id !== track.id);
+    setResults(newTrack);
+    setMyPlaylist([...myPlaylist, track]);
+  };
+
+  const deleteItem = (listItem) => {
+    const newTrack = myPlaylist.filter((list) => list.id !== listItem.id);
+    setMyPlaylist(newTrack);
+    setResults([...results, listItem]);
   };
 
   return (
-    <div>
+    <>
       <h1 className="app-title">
         Ja<span className="trademark">mmm</span>ing
       </h1>
@@ -29,18 +41,31 @@ function App() {
       <button onClick={handleSearch}>Search</button>
       <br />
       <br />
-      <ul className="results-list">
-        <h2>Results</h2>
-        {results.map((track) => (
-          <li key={track.id} className="track-item">
-            <div>
-              {track.name} by {track.artist}
-            </div>
-            <button>+</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="display-container">
+        <ul className="results-list">
+          <h2>Results</h2>
+          {results.map((track) => (
+            <li key={track.id} className="track-item">
+              <div>
+                {track.name} by {track.artist}
+              </div>
+              <button onClick={() => addItem(track)}>+</button>
+            </li>
+          ))}
+        </ul>
+        <ul className="results-list">
+          <h2>My Playlist</h2>
+          {myPlaylist.map((list) => (
+            <li key={list.id} className="track-item">
+              <div>
+                {list.name} by {list.artist}
+              </div>
+              <button onClick={() => deleteItem(list)}>-</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
